b25c302b402b8ec3cbd2e3285096bec8f94216a1
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-03-09 19:39:17 +0200

    fix: rename trip endpoint to /list and enhance query parameters with examples

--- app/routes/trips.py
+++ app/routes/trips.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from fastapi_cache.decorator import cache
 
 from app.dependencies import trips_repo
@@ -21,13 +21,13 @@ async def create_trip(trip: Trip):
     return context
 
 
-@router.get("/trip", status_code=200)
+@router.get("/list", status_code=200)
 @cache(expire=60)
 async def get_trip_list(
-        departure_station_code: int,
-        arrival_station_code: int,
-        departure_time: datetime,
-        arrival_time: datetime
+        departure_station_code: int = Query(..., example=2200001),
+        arrival_station_code: int = Query(..., example=2200002),
+        departure_time: datetime = Query(..., example="2023-04-09T14:07:13.672Z"),
+        arrival_time: datetime = Query(..., example="2026-04-09T14:07:13.672Z")
 ):
     trips_list = await trips_repo.list(
         departure_station_code,
@@ -45,5 +45,5 @@ async def get_trip_list(
             "arrival_time": trip.arrival_time
         }
         for trip in trips_list
-    ]
+    ] if trips_list else [{"detail": "No trips found"}]
     return context
