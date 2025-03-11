64331b3ad086b115540c5abcf62b34c2e4dc9637
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-03-11 10:35:54 +0200

    fix: change update_train endpoint to patch wagons and accept a list of Wagon objects

--- app/routes/trains.py
+++ app/routes/trains.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi_cache.decorator import cache
 
 from app.dependencies import trains_repo
-from app.models import Train
+from app.models import Train, Wagon
 from app.utils import normalize_wagons
 
 router = APIRouter(prefix="/train", tags=["trains"])
@@ -25,9 +25,10 @@ async def create_train(train: Train):
     return context
 
 
-@router.put("/{number}", status_code=200)
-async def update_train(number: str, train: Train):
-    renumbered_wagons = await normalize_wagons(train)
+@router.patch("/{number}", status_code=200)
+async def update_train_wagons(number: str, wagons: list[Wagon]):
+    train = await trains_repo.get(number)
+    renumbered_wagons = await normalize_wagons(Train(number=number, wagons=wagons))
     train = await trains_repo.update(number, renumbered_wagons)
     context = {"number": train.number, "wagons": [wagon.number for wagon in train.wagons]}
     return context
